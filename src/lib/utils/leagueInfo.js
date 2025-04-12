2ac92209dbb8eaebe43ae7b3069d3ae178b4b669
emilioanaya <emilio_anaya@outlook.com>
2025-04-12 16:04:19 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -168,7 +168,7 @@ export const managers = [
       "name": "Bolo",
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "California", // (optional)
-      "bio": "Lorem ipsum...",
+      "bio": "Age 22",
       "photo": "/managers/bolo.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": null, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "dal", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
@@ -181,7 +181,7 @@ export const managers = [
       "favoritePlayer": 6786, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-      "philosophy": "Your fantasy team's philosophy",
+      "philosophy": "After winning the championship in TBD's inagural season (2023), I decided I had no team outside of my core players. That offseason, I decided to tear it all down and aim for the first overall pick this year. It worked perfectly, and I now have the 1.03, and 5 more firsts over the next 3 years. I am looking to continue building around my cornerstones and look to compete soon. Look out TBD, Los Azules is coming for our second chip!",
       "tradingScale": 9, // 1 - 10
       "preferredContact": "Sleeper", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
