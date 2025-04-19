04d316920e5894a8ef341fa25429d4b1b06bea55
emilioanaya <emilio_anaya@outlook.com>
2025-04-19 15:57:03 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -183,6 +183,8 @@ export const managers = [
       "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "2023: (6-8) (1st)",
       "philosophy24": "2024: (4-10) (10th)",
+      "philosophy25": null,
+      "philosophy26": null,
       "tradingScale": 9, // 1 - 10
       "preferredContact": "Sleeper", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
