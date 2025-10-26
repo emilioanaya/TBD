d8c77ade17dd8e356b010636efacd41df097bd8f
emilioanaya <emilio_anaya@outlook.com>
2025-10-26 07:02:57 -0700

    Update leagueInfo.js
    izzy rebuild

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -170,7 +170,7 @@ export const managers = [
       "photo": "/managers/izzy.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": null, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "sf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-      "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+      "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Luke", // Can be anything (usually your rival's name)
         link: 7, // manager array number within this array, or null to link back to all managers page
