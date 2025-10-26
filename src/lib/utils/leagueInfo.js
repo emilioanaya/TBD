177d190228bcacf91703bb6a3d90ac3ca98a2f52
emilioanaya <emilio_anaya@outlook.com>
2025-10-26 07:04:26 -0700

    Update leagueInfo.js
    logan win now

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -116,7 +116,7 @@ export const managers = [
       "photo": "/managers/logan.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": null, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "sf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-      "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+      "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Bolo", // Can be anything (usually your rival's name)
         link: 6, // manager array number within this array, or null to link back to all managers page
