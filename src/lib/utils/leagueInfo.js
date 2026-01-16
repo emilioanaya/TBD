4276a45f528f4104c8dca03e807f9a84983d55a8
emilioanaya <emilio_anaya@outlook.com>
2026-01-15 22:43:42 -0800

    Update leagueInfo.js
    Updated to 2026 TBD league ID

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1181007731724742656"; // your league ID
+export const leagueID = "1312126061173043200"; // your league ID
 export const leagueName = "The Big Dynasty"; // your league name
 export const dues = 75; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
