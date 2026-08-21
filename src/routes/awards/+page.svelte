5997757ef95ab6b0960897958462a553ac4a9bd6
emilioanaya <emilio_anaya@outlook.com>
2026-08-21 15:27:15 -0700

    Update +page.svelte

--- src/routes/awards/+page.svelte
+++ src/routes/awards/+page.svelte
@@ -11,43 +11,57 @@
     .awards {
         display: block;
         margin: 30px auto;
-		width: 95%;
-		max-width: 1000px;
-		position: relative;
-		z-index: 1;
-		overflow-y: hidden;
+        width: 95%;
+        max-width: 1000px;
+        position: relative;
+        z-index: 1;
+        overflow-y: hidden;
     }
 
-	.loading {
-		display: block;
-		width: 85%;
-		max-width: 500px;
-		margin: 80px auto;
-	}
+    .loading {
+        display: block;
+        width: 85%;
+        max-width: 500px;
+        margin: 80px auto;
+    }
 
-	.nothingYet {
-		display: block;
-		width: 85%;
-		max-width: 500px;
-		margin: 80px auto;
-		text-align: center;
-	}
+    .nothingYet {
+        display: block;
+        width: 85%;
+        max-width: 500px;
+        margin: 80px auto;
+        text-align: center;
+    }
 </style>
 
 <div class="awards">
-	{#await waitForAll(awardsData, teamManagersData) }
-		<div class="loading">
-			<p>Retrieving awards data...</p>
-			<LinearProgress indeterminate />
-		</div>
-	{:then [podiums, leagueTeamManagers] }
-		{#each podiums as podium}
-			<Awards {podium} {leagueTeamManagers} />
-		{:else}
-			<p class="nothingYet">No seasons have been completed yet, so no awards have been earned...</p>
-		{/each}
-	{:catch error}
-		<!-- promise was rejected -->
-		<p>Something went wrong: {error.message}</p>
-	{/await}
+
+    {#await waitForAll(awardsData, teamManagersData)}
+
+        <div class="loading">
+            <p>Retrieving championship history...</p>
+            <LinearProgress indeterminate />
+        </div>
+
+    {:then [podiums, leagueTeamManagers]}
+
+        {#if podiums.length}
+
+            <Awards {podiums} {leagueTeamManagers} />
+
+        {:else}
+
+            <p class="nothingYet">
+                No seasons have been completed yet, so no championships
+                have been earned...
+            </p>
+
+        {/if}
+
+    {:catch error}
+
+        <p>Something went wrong: {error.message}</p>
+
+    {/await}
+
 </div>
