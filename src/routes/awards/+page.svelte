5a2fa199ee3dc369f5c0da2098aace9b069a997e
emilioanaya <emilio_anaya@outlook.com>
2026-08-21 15:56:11 -0700

    Update +page.svelte

--- src/routes/awards/+page.svelte
+++ src/routes/awards/+page.svelte
@@ -1,67 +1,75 @@
 <script>
-	import { Awards } from '$lib/components'
+	import { Awards } from '$lib/components';
 	import { waitForAll } from '$lib/utils/helper';
 	import LinearProgress from '@smui/linear-progress';
 
-    export let data;
-    const { awardsData, teamManagersData } = data;
+	export let data;
+
+	const { awardsData, teamManagersData } = data;
 </script>
 
 <style>
-    .awards {
-        display: block;
-        margin: 30px auto;
-        width: 95%;
-        max-width: 1000px;
-        position: relative;
-        z-index: 1;
-        overflow-y: hidden;
-    }
+	.awards {
+		display: block;
+		margin: 30px auto;
+		width: 95%;
+		max-width: 1000px;
+		position: relative;
+		z-index: 1;
+		overflow-y: hidden;
+	}
 
-    .loading {
-        display: block;
-        width: 85%;
-        max-width: 500px;
-        margin: 80px auto;
-    }
+	.loading {
+		display: block;
+		width: 85%;
+		max-width: 500px;
+		margin: 80px auto;
+	}
 
-    .nothingYet {
-        display: block;
-        width: 85%;
-        max-width: 500px;
-        margin: 80px auto;
-        text-align: center;
-    }
+	.nothingYet {
+		display: block;
+		width: 85%;
+		max-width: 500px;
+		margin: 80px auto;
+		text-align: center;
+	}
 </style>
 
 <div class="awards">
 
-    {#await waitForAll(awardsData, teamManagersData)}
+	{#await waitForAll(awardsData, teamManagersData)}
 
-        <div class="loading">
-            <p>Retrieving championship history...</p>
-            <LinearProgress indeterminate />
-        </div>
+		<div class="loading">
+			<p>Retrieving championship history...</p>
+			<LinearProgress indeterminate />
+		</div>
 
-    {:then [podiums, leagueTeamManagers]}
+	{:then [podiums, leagueTeamManagers]}
 
-        {#if podiums.length}
+		{#if podiums.length}
 
-            <Awards {podiums} {leagueTeamManagers} />
+			{#each podiums as podium}
 
-        {:else}
+				<Awards
+					{podium}
+					{leagueTeamManagers}
+				/>
 
-            <p class="nothingYet">
-                No seasons have been completed yet, so no championships
-                have been earned...
-            </p>
+			{/each}
 
-        {/if}
+		{:else}
 
-    {:catch error}
+			<p class="nothingYet">
+				No seasons have been completed yet, so no championships
+				have been earned...
+			</p>
 
-        <p>Something went wrong: {error.message}</p>
+		{/if}
 
-    {/await}
+	{:catch error}
+
+		<p>Something went wrong: {error.message}</p>
+
+	{/await}
 
 </div>
