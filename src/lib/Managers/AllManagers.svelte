cfc9a83e212c860d86facf85e9e777e3f87e7a51
emilioanaya <emilio_anaya@outlook.com>
2026-08-21 20:55:31 -0700

    Update AllManagers.svelte

--- src/lib/Managers/AllManagers.svelte
+++ src/lib/Managers/AllManagers.svelte
@@ -1,49 +1,64 @@
 <script>
-    import { leagueName } from '$lib/utils/helper';
-    import ManagerRow from './ManagerRow.svelte'
+	import { leagueName } from '$lib/utils/helper';
+	import ManagerRow from './ManagerRow.svelte';
 
-    export let managers, leagueTeamManagers;
+	export let managers;
+	export let leagueTeamManagers;
+	export let awards;
 
-    let innerWidth;
+	let innerWidth;
 </script>
 
 <svelte:window bind:innerWidth={innerWidth} />
 
 <style>
-    .managerContainer {
-        width: 100%;
-        margin: 2em 0 5em;
-    }
+	.managerContainer {
+		width: 100%;
+		margin: 2em 0 5em;
+	}
 
-    .managerConstrained {
-        width: 97%;
-        max-width: 800px;
-        margin: 0 auto;
-    }
+	.managerConstrained {
+		width: 97%;
+		max-width: 800px;
+		margin: 0 auto;
+	}
 
-    h2 {
-        text-align: center;
-        font-size: 2.8em;
-        margin: 2em 0 1.5em;
-        line-height: 1em;
-    }
+	h2 {
+		text-align: center;
+		font-size: 2.8em;
+		margin: 2em 0 1.5em;
+		line-height: 1em;
+	}
 
-    @media (max-width: 520px) {
-        h2 {
-            text-align: center;
-            font-size: 2em;
-            margin: 1.5em 0 1em;
-            line-height: 1em;
-        }
-    }
+	@media (max-width: 520px) {
+		h2 {
+			text-align: center;
+			font-size: 2em;
+			margin: 1.5em 0 1em;
+			line-height: 1em;
+		}
+	}
 </style>
 
 <div class="managerContainer">
-    <h2>{leagueName} Managers</h2>
-    <div class="managerConstrained">
-        {#each managers as manager, key}
-            <ManagerRow {manager} {leagueTeamManagers} {key} />
-        {/each}
-    </div>
+
+	<h2>
+		{leagueName} Managers
+	</h2>
+
+	<div class="managerConstrained">
+
+		{#each managers as manager, key}
+
+			<ManagerRow
+				{manager}
+				{leagueTeamManagers}
+				{awards}
+				{key}
+			/>
+
+		{/each}
+
+	</div>
 
 </div>
