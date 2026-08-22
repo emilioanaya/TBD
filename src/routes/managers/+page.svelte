9d38a687098b39a92cb9ffc1f42723058d8fad61
emilioanaya <emilio_anaya@outlook.com>
2026-08-21 20:55:17 -0700

    Update +page.svelte

--- src/routes/managers/+page.svelte
+++ src/routes/managers/+page.svelte
@@ -1,17 +1,23 @@
 <script>
 	import LinearProgress from '@smui/linear-progress';
-    import {AllManagers} from '$lib/components';
+	import { AllManagers } from '$lib/components';
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
+	import { waitForAll } from '$lib/utils/helper';
 
 	export let data;
-	const {managers, leagueTeamManagersData} = data;
 
-    onMount(() => {
-        if(!managers.length) {
-            goto('/');
-        }
-    })
+	const {
+		managers,
+		leagueTeamManagersData,
+		awardsData
+	} = data;
+
+	onMount(() => {
+		if (!managers.length) {
+			goto('/');
+		}
+	});
 </script>
 
 <style>
@@ -19,27 +25,51 @@
 		position: relative;
 		z-index: 1;
 	}
-    .loading {
-        display: block;
-        width: 85%;
-        max-width: 500px;
-        margin: 80px auto;
-    }
+
+	.loading {
+		display: block;
+		width: 85%;
+		max-width: 500px;
+		margin: 80px auto;
+	}
 </style>
 
 <div class="main">
-    {#await leagueTeamManagersData}
-        <!-- promise is pending -->
-        <div class="loading">
-            <p>Retrieving managers...</p>
-            <LinearProgress indeterminate />
-        </div>
-    {:then leagueTeamManagers}
-        {#if managers.length}
-            <AllManagers {managers}  {leagueTeamManagers}/>
-        {/if}
-    {:catch error}
-        <!-- promise was rejected -->
-        <p>Something went wrong: {error.message}</p>
-    {/await}
+
+	{#await waitForAll(
+		leagueTeamManagersData,
+		awardsData
+	)}
+
+		<div class="loading">
+
+			<p>
+				Retrieving managers...
+			</p>
+
+			<LinearProgress indeterminate />
+
+		</div>
+
+	{:then [leagueTeamManagers, awards]}
+
+		{#if managers.length}
+
+			<AllManagers
+				{managers}
+				{leagueTeamManagers}
+				{awards}
+			/>
+
+		{/if}
+
+	{:catch error}
+
+		<p>
+			Something went wrong:
+			{error.message}
+		</p>
+
+	{/await}
+
 </div>
